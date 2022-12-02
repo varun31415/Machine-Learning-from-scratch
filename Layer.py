fe56756b0645212acc43edd4999653b602696930
Varun Rajkumar <pi.fermet@gmail.com>
2022-12-01 22:37:16 -0800

    added comments for better understanding

--- Layer.py
+++ Layer.py
@@ -2,30 +2,37 @@ import numpy as np
 
 class Layer:
     def __init__(self, neurons, next_layer_nuerons):
+        # weights and biases 
         self.weights = np.random.rand(neurons, next_layer_nuerons) - 0.5
         self.biases = np.random.rand(1, next_layer_nuerons) - 0.5
 
     def forward(self, input_data):
+        # matrix multiplication
         self.input_before = input_data
         self.input_after = np.dot(self.input_before,self.weights) + self.biases
+
+        #activation
         self.output = self.tanh(self.input_after)
+
         return self.output
 
-    def sigmoid_prime(self, x):
-        return np.exp(-x)/(np.exp(-x) + 1)**2
-
     def tanh(self, x):
+        # activation function
         return np.tanh(x)
     
     def tanh_prime(self, x):
+        # activation derivative
         return 1-np.tanh(x)**2
 
     def backward(self, error, learning_rate):
+        # activation error calculation
         output_error = self.tanh_prime(self.input_after) * error
 
+        # calculate error
         input_error = np.dot(output_error, self.weights.T)
         weights_error = np.dot(self.input_before.T, output_error)
 
+        # adjust weights and biases
         self.weights -= learning_rate * weights_error
         self.biases -= learning_rate * output_error
 
--- Network.py
+++ Network.py
@@ -7,19 +7,19 @@ class Network:
     def add(self, layer):
         self.layers.append(layer)
 
-    def loss(output, prediction):
-        return np.mean(np.power(output-prediction, 2))
-
     def predict(self, input_layer):
         output = input_layer
+        #forward prop
         for layer in self.layers:
             output = layer.forward(output)
         return output
 
     def loss(self, y_true, y_pred):
+        # loss
         return np.mean(np.power(y_true-y_pred, 2))
 
     def loss_prime(self, y_true, y_pred):
+        # loss prime
         return 2*(y_pred-y_true)/y_true.size
     
     def train(self, x_train, y_train, epochs, learning_rate):
@@ -28,9 +28,11 @@ class Network:
         for i in range(epochs):
             for j in range(samples):
                 output = x_train[j]
+                # calculate output
                 for layer in self.layers:
                     output = layer.forward(output)
                 
+                # back prop
                 error = self.loss_prime(y_train[j], output)
                 for layer in reversed(self.layers):
                     error = layer.backward(error, learning_rate)
